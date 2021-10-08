2b2e0a4f6bfa0aae4db7158e35313fd4c52006d4
Naman Singh <55326403+mello369@users.noreply.github.com>
2021-10-08 15:26:01 +0530

    Add files via upload

---
+++ lab1/blink.ino
@@ -0,0 +1,14 @@
+int pin=13;
+int d=1000;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pin,HIGH);
+  delay(d);
+   digitalWrite(pin,LOW);
+   delay(d);
+}
